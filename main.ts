62956c01957984c059f906d0cac4c68088c281cf
sadiakanwal10 <sadiakanwal715@gmail.com>
2024-06-27 23:40:50 -0700

    first commit

---
+++ main.ts
@@ -0,0 +1,31 @@
+// question no 07 //
+// ADDING 2 VALUES TO GET 8 //
+let addition = 5+3;
+console.log("ADDITION RESULT:", addition);
+
+// SUBTRACTING 2 VALUES TO GET 8//
+let subtraction= 13-5;
+console.log("SUBTRACTION RESULT", subtraction);
+
+// MULTIPLICATING 2 VALUES TO GET 8 //
+let multiplication=2*4;
+console.log("MULTIPLICATION:", multiplication);
+
+//DIVIDING 2 VALUES TO GET 8 //
+let division= 16/2;
+console.log("DIVISION RESULT:", division);
+
+// question no 08 //
+// ADDING , SUBTRACTING , MULTIPLICATING , DIVIDING 2 VALUES TO GET 8 AND DIRECTLY PRINT IT //
+console.log(5+3);
+console.log(13-5);
+console.log(2*4);
+console.log(16/2);
+
+// question no 09 //
+// STORE MY FAVOURITE NO IN A VARIABLE //
+let myFavouriteNumber= 10;
+// STORE A MESSAGE IN A VARIABLE THAT SHOWS MY FAVOURITE NO //
+let message=`"MY FAVOURITE NUMBER IS 10"`;
+// PRINT THE MESSAGE //
+console.log(message);
